ae18b0c7e960e42ae3c75ec99f208cd1f30ba242
skkif1 <skkif1@gmail.com>
2017-03-25 00:55:28 +0200

    final

--- fdf.h
+++ fdf.h
@@ -13,47 +13,47 @@
 #ifndef FDF_H
 # define FDF_H
 
-#include "libft/ft_printf.h"
-#include "mlx.h"
-#include <errno.h>
-#include <math.h>
-
-#define PI 3.1415926
-#define COSNS(ang) cos(ang * PI / 180)
-#define SINSS(ang) sin(ang * PI / 180)
+# include "libft/ft_printf.h"
+# include "mlx.h"
+# include <errno.h>
+# include <math.h>
+# define PI 3.1415926
 
 typedef struct	s_point
 {
-    int y;
-    int x;
-    int height;
-    int color;
+	int y;
+	int x;
+	int height;
+	int color;
+	int deltax;
+	int deltay;
+	int sign_x;
+	int sign_y;
 }				t_point;
-
 
 typedef struct	s_map
 {
-    t_point map[2000][2000];
-    t_point copy[20000][20000];
-    int width;
-    int height;
-    void *mlx;
-    void *window;
-    int scale;
-    double angle_x;
-    double angle_y;
-    double angle_z;
-    int map_max;
+	t_point	map[2000][2000];
+	t_point	copy[20000][20000];
+	int		width;
+	int		height;
+	void	*mlx;
+	void	*window;
+	int		scale;
+	double	angle_x;
+	double	angle_y;
+	double	angle_z;
+	int		map_max;
 }				t_map;
 
-
-int get_map(char *name, t_map *map);
-void reculc_point(t_map *map);
-int key_listener(int keycode, void *param);
-void draw_map(t_map *map);
-void reculc_map(t_map *map, int vector);
-void make_copy(t_map *map);
-int normal_round(double num);
-
+int				get_map(char *name, t_map *map);
+void			reculc_point(t_map *map);
+int				key_listener(int keycode, void *param);
+void			draw_map(t_map *map);
+void			reculc_map(t_map *map);
+void			make_copy(t_map *map);
+void			cust_exit(t_map *map, const char *str);
+void			change_color(t_map *map);
+void			free_list(t_list *lst);
 
 #endif
